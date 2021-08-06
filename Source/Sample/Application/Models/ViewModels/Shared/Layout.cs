6f921e37b330ccb36a134bebfbf551892d63a53b
HansKindberg <hans.kindberg@gmail.com>
2021-08-06 10:18:00 +0200

    Sample: code-analysis-fix

--- Source/Sample/Application/Models/ViewModels/Shared/Layout.cs
+++ Source/Sample/Application/Models/ViewModels/Shared/Layout.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using RegionOrebroLan.DependencyInjection;
 
 namespace Application.Models.ViewModels.Shared
 {
 	[ServiceConfiguration(Lifetime = ServiceLifetime.Scoped, ServiceType = typeof(ILayout))]
+	[SuppressMessage("Naming", "CA1724:Type names should not match namespaces")]
 	public class Layout : ILayout
 	{
 		#region Constructors
